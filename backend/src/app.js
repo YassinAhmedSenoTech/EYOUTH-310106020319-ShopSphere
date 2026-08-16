5339ec74a702045297731a0f1aeb7bb12e432afb
Genius <yasso.ahmed.nasf@gmail.com>
2026-08-16 03:27:22 +0300

    Fix vercel paths and trust proxy

--- backend/src/app.js
+++ backend/src/app.js
@@ -112,9 +112,10 @@ import reviewRoutes from './routes/reviewRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 
 const app = express();
+app.set('trust proxy', 1);
+
 const PORT = process.env.PORT || 5000;
 
-app.set('trust proxy', 1);
 
 
 
